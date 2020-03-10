8e3a78f53e8cf7a36b7289ec4e43aaac7bc8f876
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-03-10 17:16:42 -0600

    Action RPG series part 1.

---
+++ Action RPG/Player/Player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_collide(velocity)
