2278c92d718a721b5750574e26c67bbaaf7b9895
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-03-16 15:35:16 -0600

    Godot Action RPG P3 - Collisions

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10
-const MAX_SPEED = 100
-const FRICTION = 10
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
@@ -13,9 +13,8 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
