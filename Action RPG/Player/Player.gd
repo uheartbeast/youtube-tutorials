0f57f6347f27c7783360ce843e4c7dcd241db2d8
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-03-18 14:17:47 -0600

    Godot Action RPG Part 5 (animationPlayer + Onready var)

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -6,6 +6,8 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +15,13 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
