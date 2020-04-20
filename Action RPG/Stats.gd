ffc30a511e9fa7ecf068f3f230f14ed778ce837c
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-04-20 11:14:23 -0600

    Godot Action RPG Part 17. Player hearts

--- Action RPG/Stats.gd
+++ Action RPG/Stats.gd
@@ -1,11 +1,22 @@
 extends Node
 
-export(int) var max_health = 1
-onready var health = max_health setget set_health
+export(int) var max_health = 1 setget set_max_health
+var health = max_health setget set_health
 
 signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
+
+func set_max_health(value):
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
 
 func set_health(value):
 	health = value
+	emit_signal("health_changed", health)
 	if health <= 0:
 		emit_signal("no_health")
+
+func _ready():
+	self.health = max_health
---
+++ Action RPG/UI/HealthUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var hearts = 4 setget set_hearts
+var max_hearts = 4 setget set_max_hearts
+
+onready var heartUIFull = $HeartUIFull
+onready var heartUIEmpty = $HeartUIEmpty
+
+func set_hearts(value):
+	hearts = clamp(value, 0, max_hearts)
+	if heartUIFull != null:
+		heartUIFull.rect_size.x = hearts * 15
+
+func set_max_hearts(value):
+	max_hearts = max(value, 1)
+	self.hearts = min(hearts, max_hearts)
+	if heartUIEmpty != null:
+		heartUIEmpty.rect_size.x = max_hearts * 15
+
+func _ready():
+	self.max_hearts = PlayerStats.max_health
+	self.hearts = PlayerStats.health
+	PlayerStats.connect("health_changed", self, "set_hearts")
+	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
