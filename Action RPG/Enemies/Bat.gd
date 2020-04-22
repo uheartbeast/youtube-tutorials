19ad4ab1ec0866de61d3af7b475b4baf028066e6
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-04-22 11:53:55 -0600

    Added soft collisions.

--- Action RPG/Enemies/Bat.gd
+++ Action RPG/Enemies/Bat.gd
@@ -21,6 +21,7 @@ onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
+onready var softCollision = $SoftCollision
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -43,6 +44,8 @@ func _physics_process(delta):
 				state = IDLE
 			sprite.flip_h = velocity.x < 0
 
+	if softCollision.is_colliding():
+		velocity += softCollision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
 
 func seek_player():
---
+++ Action RPG/Overlap/SoftCollision.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+func is_colliding():
+	var areas = get_overlapping_areas()
+	return areas.size() > 0
+
+func get_push_vector():
+	var areas = get_overlapping_areas()
+	var push_vector = Vector2.ZERO
+	if is_colliding():
+		var area = areas[0]
+		push_vector = area.global_position.direction_to(global_position)
+		push_vector = push_vector.normalized()
+	return push_vector
